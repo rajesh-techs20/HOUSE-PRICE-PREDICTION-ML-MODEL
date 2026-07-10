d5045bb32ebae76d972ebe979941824f2ed238d6
RAJESH G R <rgr98624@gmail.com>
2026-07-10 19:16:23 +0530

    Implement train-test split and model evaluation
    Added model evaluation metrics and train-test split.

--- text.py
+++ text.py
@@ -1,17 +1,30 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
+from sklearn.metrics import mean_squared_error,r2_score
 
-df=pd.read_csv("house_data.csv", engine="python")
+df=pd.read_csv("house_data.csv",engine="python")
 
 X=df[["Area","Bedrooms","Bathrooms","Age","Parking"]]
 y=df["Price"]
 
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+                                               
 model=LinearRegression()
-model.fit(X,y)
+model.fit(X_train,y_train)
+
+y_pred=model.predict(X_test)
+
+mse=mean_squared_error(y_test,y_pred)
+r2=r2_score(y_test,y_pred)
+
+print("Mean Square Error:",mse)
+print("R2 Score:",r2)
+
 
 with open("model.pkl","wb") as file:
     pickle.dump(model,file)
 
-print("Model is trained and saved as model.pkl")
+print("Model is trained,evaluated successfully and saved as model.pkl")
